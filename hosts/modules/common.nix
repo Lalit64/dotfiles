4d6bbbb57037a8feca8d52141a47f345422ef2e3
Lalit Yalamanchili <lalit.yalamanchili@gmail.com>
2025-01-21 17:57:54 +0530

    disable .DS_Store

--- hosts/modules/common.nix
+++ hosts/modules/common.nix
@@ -41,6 +41,9 @@
        # Wallpaper
        echo >&2 "Setting the wallpaper..."
       osascript -e 'tell application "Finder" to set desktop picture to POSIX file "${wallpaper}"'
+      # disable .DS_Store files
+      defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool TRUE
+      defaults write com.apple.desktopservices DSDontWriteUSBStores -bool TRUE
     '';
 
     defaults = {
